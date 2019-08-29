e8b7d4107273770f88cebfdd4d6b6f3d7376e957
elixirff11 <50103193+elixirff11@users.noreply.github.com>
2019-08-29 18:55:05 +0900

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+package "vim-enhanced" do
+  action :install
+end
+
+%w{php mysql-server httpd}.each do |p|
+  package p do
+    action :install
+  end
+end
+
+service "httpd" do
+  action [:start, :enable]
+end
